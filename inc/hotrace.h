7f4be46f59148eb01db180639c6b7a0d2e6f9149
MathTheMith <mvachon@student.42lyon.fr>
2026-03-01 13:45:36 +0100

    add: norm done

--- inc/hotrace.h
+++ inc/hotrace.h
@@ -6,38 +6,38 @@
 /*   By: mvachon <mvachon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/28 14:19:50 by lud-adam          #+#    #+#             */
-/*   Updated: 2026/03/01 09:33:40 by mvachon          ###   ########.fr       */
+/*   Updated: 2026/03/01 12:39:28 by mvachon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HOTRACE_H
-#define HOTRACE_H
+# define HOTRACE_H
 
-#include <stdlib.h>
-#include <stdbool.h>
+# define MAX_LOAD 0.75
+# include <stdlib.h>
+# include <stdbool.h>
 
-#define MAX_LOAD 0.75
-
-typedef struct
+typedef struct hash
 {
-	void* 	array;
-	int 	count;
+	void	*array;
+	int		count;
 	int		capacity;
 	int		new_capacity;
 	size_t	key_size;
 	size_t	value_size;
 
-	bool	(*cmp)(const void*, const void*);
-} hashmap;
+	bool	(*cmp)(const void *, const void *);
+}	t_hashmap;
 
-bool str_cmp(const void* a, const void* b);
-void	init_map(hashmap *map, size_t key_size, size_t value_size, bool (*cmp)(const void*, const void*));
-bool map_put(hashmap *map, void *key, void *value);
-void *map_get(hashmap *map, void *key);
-void mapClear(hashmap *map);
-void	free_map(hashmap *map);
-void    *ft_realloc(void *ptr, size_t new_size);
+bool	str_cmp(const void *a, const void *b);
+void	init_map(t_hashmap *map, size_t key_size,
+			size_t value_size, bool (*cmp)(const void*, const void*));
+bool	map_put(t_hashmap *map, void *key, void *value);
+void	*map_get(t_hashmap *map, void *key);
+void	map_clear(t_hashmap *map);
+void	free_map(t_hashmap *map);
+void	*ft_realloc(void *ptr, size_t new_size);
 void	*ft_memset(void *pointer, int value, size_t count);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
-bool str_cmp(const void* a, const void* b);
+bool	str_cmp(const void *a, const void *b);
 #endif
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mvachon <mvachon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/28 08:15:28 by mvachon           #+#    #+#             */
-/*   Updated: 2026/03/01 10:13:41 by mvachon          ###   ########.fr       */
+/*   Updated: 2026/03/01 13:44:23 by mvachon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,71 +16,75 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdlib.h> 
-
 #include "hotrace.h"
 
 #define BUFFER_SIZE 4096
 
-
-char *read_line(void)
+char	*read_line(void)
 {
-    static char buffer[BUFFER_SIZE];
-    static int  buf_size = 0;
-    static int  buf_pos = 0;
-    char        line[4096];
-    int         line_len = 0;
+	static char	buffer[BUFFER_SIZE];
+	static int	buf_size = 0;
+	static int	buf_pos = 0;
+	char		line[4096];
+	int			line_len;
+	char		*result;
 
-    while (1)
-    {
-        if (buf_pos >= buf_size)
-        {
-            buf_size = read(0, buffer, BUFFER_SIZE);
-            buf_pos = 0;
-            if (buf_size <= 0)
-                return (NULL);
-        }
-        if (buffer[buf_pos] == '\n')
-        {
-            buf_pos++;
-            break;
-        }
-        line[line_len++] = buffer[buf_pos++];
-    }
-    line[line_len] = '\0';
-    if (line_len == 0)
-        return (NULL);
-
-    char *result = malloc(line_len + 1);
-    ft_memcpy(result, line, line_len + 1);
-    return (result);
+	line_len = 0;
+	while (1)
+	{
+		if (buf_pos >= buf_size)
+		{
+			buf_size = read(0, buffer, BUFFER_SIZE);
+			buf_pos = 0;
+			if (buf_size <= 0)
+				return (NULL);
+		}
+		if (buffer[buf_pos] == '\n')
+		{
+			buf_pos++;
+			break ;
+		}
+		line[line_len++] = buffer[buf_pos++];
+	}
+	line[line_len] = '\0';
+	if (line_len == 0)
+		return (NULL);
+	result = malloc(line_len + 1);
+	ft_memcpy(result, line, line_len + 1);
+	return (result);
 }
 
-void read_data(hashmap *map)
+void	read_data(t_hashmap *map)
 {
-	while(1)
-	{
-		char *key = read_line();;
-		if (!key || key[0] == '\0'|| key[0] == '\n')
-			break;
+	char	*key;
+	char	*value;
 
-		char *value= read_line();
+	while (1)
+	{
+		key = read_line();
+		if (!key || key[0] == '\0' || key[0] == '\n')
+			break ;
+		value = read_line();
 		if (!value || key[0] == '\n')
-			break;
+			break ;
 		map_put(map, &key, &value);
 	}
 }
 
-void read_searches(hashmap *map)
+void	read_searches(t_hashmap *map)
 {
-	char *line;
+	char	*line;
+	char	*key;
+	char	**result;
 
+	result = NULL;
 	line = NULL;
-	while(1)
+	while (1)
 	{
-		char *key = read_line();
-		if (!key || key[0] == '\0'|| key[0] == '\n')
-			break;
-		char **result = map_get(map, &key);
+		key = read_line();
+		if (!key || key[0] == '\0' || key[0] == '\n')
+			break ;
+		result = map_get(map, &key);
 		if (result)
 			printf("Value for '%s' = %s\n", key, *result);
 		else
@@ -89,15 +93,15 @@ void read_searches(hashmap *map)
 	}
 }
 
-int main(void)
+int	main(void)
 {
-    hashmap map;
-    char    *line;
+	t_hashmap	map;
+	char		*line;
 
-    line = NULL;
-    init_map(&map, sizeof(char *), sizeof(char *), str_cmp);
-    read_data(&map);
-    read_searches(&map);
-    mapClear(&map);
-    return 0;
+	line = NULL;
+	init_map(&map, sizeof(char *), sizeof(char *), str_cmp);
+	read_data(&map);
+	read_searches(&map);
+	map_clear(&map);
+	return (0);
 }
